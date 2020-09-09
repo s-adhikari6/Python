d253c6fdf8c0845ae995881cc05a62faadf4cc66
s-adhikari6 <70979425+s-adhikari6@users.noreply.github.com>
2020-09-09 14:12:15 -0600

    Add files via upload

---
+++ Python/hello.py
@@ -0,0 +1,34 @@
+# print ("Area od Triangle Calc")
+
+# b = float(input("Base of triangle:"))
+# h = float(input("Height of triangle:"))
+
+# ans = ( b * h ) / 2
+
+# print (ans)
+
+
+
+# for i in range(11):
+#     for j in range(i + 1):
+#         print (j, end=" ")
+#     print ()
+
+
+
+x = []
+i = 0
+while i < 10:
+    done = False
+   
+
+    while not done:
+        inp = int(input("Enter a number:"))
+        ans = input("Do you want to quit?")
+        if ans == 'y'
+            done = True
+
+        x.append(inp)
+        print(x)
+
+
